245db3737b0ca095e573bf5c15965cab859ae460
EbenZhang <zhangyingneng@gmail.com>
2019-07-27 23:31:10 +0800

    Feat: delete legacy shortcuts

--- DBRestorer/AutoUpdateFromGitHubRelease.cs
+++ DBRestorer/AutoUpdateFromGitHubRelease.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using DBRestorer.Ctrl.Domain;
 using Squirrel;
@@ -14,10 +15,36 @@ namespace DBRestorer
                 var updated = await mgr.UpdateApp(progressReport) != null;
                 if (updated)
                 {
+                    RemoveLegacyClickOnceFiles();
                     UpdateManager.RestartApp();
                 }
 
                 return updated;
+            }
+        }
+
+        private void RemoveLegacyClickOnceFiles()
+        {
+            var fileName = "DbRestorer.appref-ms";
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            var desktopShortcut = Path.Combine(desktop, fileName);
+            DeleteIfExists(desktopShortcut);
+
+            var startMenu = Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+            var startMenuShortcut = Path.Combine(startMenu, "programs", "Nicologies", fileName);
+            DeleteIfExists(startMenuShortcut);
+
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var quickLaunchFolder = Path.Combine(appData, @"Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar");
+            var quickLaunch = Path.Combine(quickLaunchFolder, fileName);
+            DeleteIfExists(quickLaunch);
+        }
+
+        private static void DeleteIfExists(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
             }
         }
     }
