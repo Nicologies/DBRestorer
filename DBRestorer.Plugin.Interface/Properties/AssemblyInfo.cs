30771ae422ffce1317e05734be5462164a411127
EbenZhang <zhangyingneng@gmail.com>
2019-07-28 09:18:22 +0800

    Fix wrong encoding char in the AssemblyInfo.cs

--- DBRestorer.Plugin.Interface/Properties/AssemblyInfo.cs
+++ DBRestorer.Plugin.Interface/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicologies")]
 [assembly: AssemblyProduct("Nicologies.DBRestorer.Plugin.Interface")]
-[assembly: AssemblyCopyright("Copyright Nicologies ©2016")]
+[assembly: AssemblyCopyright("Copyright Nicologies © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
