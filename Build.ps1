08211006db2c86556ff904937374733c9a5559e7
Eben Zhang <zhangyingneng@gmail.com>
2023-02-10 20:37:06 +0800

    Migrate to .net sdk style

--- Build.ps1
+++ Build.ps1
@@ -1,9 +1,14 @@
+param(
+    [string]$version
+)
 $ErrorActionPreference = 'Stop'
-msbuild '/t:restore;build' DBRestorer.sln /m /p:Configuration=Release
+Write-Host "version is ${version}"
+msbuild '/t:restore;build' DBRestorer.sln /m /p:Configuration=Release /p:Version=$version
 if($LastExitCode -ne 0)
 {
     $err = "Failed to build, error " + $LastExitCode.ToString()
     Write-Error $err
     Exit $LastExitCode
 }
+Write-Host "Build finished, now creating the installer"
 . $PSScriptRoot/DBRestorer/CreateInstaller.ps1
--- DBRestorer.Plugin.Interface/Properties/AssemblyInfo.cs
+++ DBRestorer.Plugin.Interface/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Nicologies.DBRestorer.Plugin.Interface")]
-[assembly: AssemblyDescription("Plugin Interface for DBRestorer")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Nicologies")]
-[assembly: AssemblyProduct("Nicologies.DBRestorer.Plugin.Interface")]
-[assembly: AssemblyCopyright("Copyright Nicologies © 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Plugin_DbRestorer")]
 [assembly: InternalsVisibleTo("DBRestorer.Ctrl")]
 
@@ -23,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("05512917-cb75-4717-aee1-b56612c31ab1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
